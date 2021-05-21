c2f83c0bd82279237c570fed7ae14bbfa6e8a7c4
iwaki1990 <low.son.wing@gmail.com>
2021-05-21 20:18:22 +0900

    3 commit

--- js/main.js
+++ js/main.js
@@ -2,15 +2,8 @@ const target = document.getElementById("menu");
 target.addEventListener('click', () => {
   const target = document.getElementById("menu");
   target.classList.toggle('open');
-  const nav = document.getElementById("nav");
-  nav.classList.toggle('in');
-});
-
-const target = document.getElementById("menu");
-target.addEventListener('click', () => {
-  const target = document.getElementById("menu");
-  target.classList.toggle('open');
   const nav = document.getElementById("drawer-nav");
   nav.classList.toggle('in');
 });
+
 
